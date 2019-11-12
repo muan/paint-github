b2aa578df1b105efb8b99ce872363c38559ab597
Mu-An Chiou <me@muanchiou.com>
2019-11-12 15:34:31 -0500

    Make work in firefox and Chrome

--- inject.js
+++ inject.js
@@ -1,6 +1,6 @@
 for (const file of ['paint-canvas-element.js', 'index.js']) {
   const script = document.createElement('script')
   script.setAttribute('type', 'module')
-  script.setAttribute('src', chrome.extension.getURL(file))
+  script.setAttribute('src', (chrome || browser).runtime.getURL(file))
   document.body.append(script)
 }
